12d879c1dad22815b0076e1e90bc63df57b575f7
Lachie King <kingy00112233@gmail.com>
2026-01-31 03:43:40 +0000

    minor updates

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for j in range(len(row)):
             if j != len(row) - 1:
-                print("{:d}".format(num), end=" ")
+                print("{:d}".format(row[j]), end=" ")
             else:
-                print("{:d}".format(num), end="")
+                print("{:d}".format(row[j]), end="")
         print()
