038ba7a1a54c69a07870ede9ad5949111838c5cb
Lachie King <kingy00112233@gmail.com>
2026-02-01 05:58:55 +0000

    update

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     count = 0
-    i = 0
-    while count < x:
+    for i in range(x):
         try:
-            print(my_list[i], end="")
+            print("{}".format(my_list[i]), end="")
             count += 1
-            i += 1
-        except IndexError:
+        except Exception:
             break
     print()
     return count
