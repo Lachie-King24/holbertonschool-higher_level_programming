8c8c42be8c4c2a78b4e1c5b27aff6459f50b462e
Lachie King <kingy00112233@gmail.com>
2026-02-01 06:36:59 +0000

    update file 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -7,8 +7,12 @@ def add_integer(a, b=98):
     Floats are casted to integers.
     Raises TypeError if a or b are not integers or floats.
     """
-    if not isinstance(a, (int, float)):
+    if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
-    if not isinstance(b, (int, float)):
+    if type(b) not in [int, float]:
         raise TypeError("b must be an integer")
-    return int(a) + int(b)
+    
+    try:
+        return int(a) + int(b)
+    except OverflowError:
+        raise TypeError("a or b too large")
