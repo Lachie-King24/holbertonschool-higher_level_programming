1eeca1995f75d1b7877e13621323944cac804d58
Lachie King <kingy00112233@gmail.com>
2026-02-01 06:35:23 +0000

    update task 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -7,8 +7,8 @@ def add_integer(a, b=98):
     Floats are casted to integers.
     Raises TypeError if a or b are not integers or floats.
     """
-    if type(a) not in [int, float]:
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    if type(b) not in [int, float]:
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
     return int(a) + int(b)
