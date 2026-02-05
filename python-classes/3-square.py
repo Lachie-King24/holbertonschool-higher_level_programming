0492f8aea38778c4b32302c3899309b54a59230d
Lachie King <kingy00112233@gmail.com>
2026-02-06 10:56:50 +1100

    add 3-square

---
+++ python-classes/3-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Define a Square with a Class"""
+
+
+class Square:
+    """Define a Square"""
+    def __init__(self, size=0):
+        self.size = size
+
+    #getter for size
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    #getter for area
+    @property
+    def area(self):
+        return self.__size * self.__size
