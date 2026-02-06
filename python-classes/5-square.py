95edd01865248256fb8843a8e4cdb4ae7c6a5390
Lachie King <kingy00112233@gmail.com>
2026-02-06 11:10:39 +1100

    add 5-square

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -28,6 +28,6 @@ class Square:
             print()
         else:
             for i in range(self.__size):
-                for j in range(self.__size): 
+                for j in range(self.__size):
                     print("#", end="")
                 print()
