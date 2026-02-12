90366bfff56c27aa5d18a32acf6270b3327e075e
Lachie King <kingy00112233@gmail.com>
2026-02-13 09:43:18 +1100

    update 0-lookup

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Define function to list attributes and methods"""
+
+
 def lookup(obj):
-    """Return list of available attributes and methods of an object"""
+    """Return list of available attributes and methods of object"""
     return dir(obj)
