d67a860a6ad4016c536027afa7c8a6489a8e723b
Lachie King <kingy00112233@gmail.com>
2026-02-13 10:31:02 +1100

    add 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines a BaseGeometry class with an area method"""
+
+class BaseGeometry:
+    """BaseGeometry class"""
+
+    def area(self):
+        """Raises an Exception indicating area is not implemented"""
+        raise Exception("area() is not implemented")
