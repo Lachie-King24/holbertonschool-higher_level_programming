20c518d61371b70bb6962ec5d7dfb2423d7a42f9
Lachie King <kingy00112233@gmail.com>
2026-02-13 10:40:27 +1100

    add 8

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+Rectangle = __import__('7-base_geometry').BaseGeometry
+
+class Rectangle(Rectangle):
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
