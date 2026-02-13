5826f55d57f5feaa18241df9d18a4d8fa3773cba
Lachie King <kingy00112233@gmail.com>
2026-02-13 11:08:36 +1100

    add task 1 and 2

---
+++ python-abc/task_00_abc.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Module that defines abstract Animal class and Dog and Cat subclasses"""
+
+
+from abc import ABC, abstractmethod
+
+class Animal(ABC):
+    """Abstract base class for animals"""
+
+    @abstractmethod
+    def sound(self):
+        """Abstract method to return the animal's sound"""
+        pass
+
+
+class Dog(Animal):
+    """Dog subclass of Animal"""
+
+    def sound(self):
+        return "Bark"
+
+
+class Cat(Animal):
+    """Cat subclass of Animal"""
+
+    def sound(self):
+        return "Meow"
