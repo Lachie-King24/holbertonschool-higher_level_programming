04b59f33d6228862703d0fa04e475d2b248b3ea5
Lachie King <kingy00112233@gmail.com>
2026-02-13 11:21:30 +1100

    Add task 2

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-"""Define Shape abstract class and its Circle and Rectangle subclasses"""
+"""Module that defines Shape, Circle, Rectangle, and shape_info"""
 
 from abc import ABC, abstractmethod
 import math
 
-# Abstract class Shape
 class Shape(ABC):
     """Abstract base class for all shapes"""
 
@@ -18,8 +17,6 @@ class Shape(ABC):
         """Return the perimeter of the shape"""
         pass
 
-
-# Circle class
 class Circle(Shape):
     """Circle shape"""
 
@@ -32,8 +29,6 @@ class Circle(Shape):
     def perimeter(self):
         return 2 * math.pi * self.radius
 
-
-# Rectangle class
 class Rectangle(Shape):
     """Rectangle shape"""
 
@@ -47,9 +42,8 @@ class Rectangle(Shape):
     def perimeter(self):
         return 2 * (self.width + self.height)
 
-
-# Duck typing function
 def shape_info(shape):
     """Print the area and perimeter of any shape"""
+    # Print exactly like Holberton expects
     print("Area:", shape.area())
     print("Perimeter:", shape.perimeter())
