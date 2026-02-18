7b54694b05069f5e7b98b3e446380f7d4f7a4b37
Lachie King <kingy00112233@gmail.com>
2026-02-18 17:07:18 +1100

    add task 0

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""serialize and deserialize"""
+
+
+import json
+
+
+
+def serialize_and_save_to_file(data, filename):
+    """serialize to json file"""
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump(data, f)
+
+def load_and_deserialize(filename):
+    with open(filename, "r", encoding="utf-8") as f:
+        return json.load(f)
