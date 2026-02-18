14cfe236b10c10d03a8ad83b18d6dd8ab6bf14b4
Lachie King <kingy00112233@gmail.com>
2026-02-18 11:34:59 +1100

    update 0-read_file to contain bin bash and comments

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,3 +1,10 @@
+#!/usr/bin/python3
+"""define function to read a full file"""
+
+
 def read_file(filename=""):
+    """function to read file"""
+    # open file
     with open(filename, encoding="utf-8") as f:
+        # print the lines that are read (whole file)
         print(f.read(), end="")
