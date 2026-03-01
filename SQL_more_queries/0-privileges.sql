d97c73c162a0e74e67eb69453e09ae9d5de84bcc
Lachie King <kingy00112233@gmail.com>
2026-03-01 04:50:31 +0000

    update 0

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -5,6 +5,8 @@ CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
 -- Optionally grant minimal privileges so SHOW GRANTS works
 GRANT USAGE ON *.* TO 'user_0d_1'@'localhost';
 
+FLUSH PRIVILEGES;
+
 -- Show the privileges for both users
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
