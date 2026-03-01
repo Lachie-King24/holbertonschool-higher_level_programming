ee7bdba619636cb6933bc6c0e484e54f4d15fe85
Lachie King <kingy00112233@gmail.com>
2026-03-01 04:35:30 +0000

    update task 0

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,12 +1,9 @@
 -- 0-privileges.sql
 -- Create users if they don't exist
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
 
 -- Optionally grant minimal privileges so SHOW GRANTS works
--- (you can adjust privileges as needed)
 GRANT USAGE ON *.* TO 'user_0d_1'@'localhost';
-GRANT USAGE ON *.* TO 'user_0d_2'@'localhost';
 
 -- Show the privileges for both users
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
