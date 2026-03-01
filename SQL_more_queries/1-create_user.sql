cb31918daa69c9d7cdd180fc0ce2ff6e0f7f4c22
Lachie King <kingy00112233@gmail.com>
2026-03-01 05:10:20 +0000

    update task 1

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -2,6 +2,6 @@
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-GRANT GRANT OPTION ON *.* TO 'user_0d_1'@'localhost';
 
-FLUSH PRIVILEGES;
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
