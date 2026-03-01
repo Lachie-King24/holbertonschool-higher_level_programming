29989899adb391d4d37a83fc6ca80af885612e0d
Lachie King <kingy00112233@gmail.com>
2026-03-01 04:52:17 +0000

    update 0

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,6 +1,7 @@
 -- 0-privileges.sql
 -- Create users if they don't exist
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
 
 -- Optionally grant minimal privileges so SHOW GRANTS works
 GRANT USAGE ON *.* TO 'user_0d_1'@'localhost';
