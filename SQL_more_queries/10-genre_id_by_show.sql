81002e1c4ef12067032f7722075d6ec6f8c52fe1
Lachie King <kingy00112233@gmail.com>
2026-03-07 12:53:26 +1100

    change task 10

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,5 @@
 -- 10-genre_id_by_show.sql
-SELECT tv_shows.title, tv_show_genres.genre_id
+SELECT tv_shows.title AS title, tv_show_genres.genre_id AS genre_id
 FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
