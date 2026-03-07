8d6848094c38a7e0aacd1b9db5e0d8663717270f
Lachie King <kingy00112233@gmail.com>
2026-03-07 11:33:47 +1100

    update task 4

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -20,7 +20,10 @@ if __name__ == "__main__":
     cur = db.cursor()
 
     # SAFE query using parameterization
-    cur.execute("SELECT * FROM cities ORDER BY id ASC")
+    cur.execute("SELECT cities.id, cities.name, states.name \
+    FROM cities \
+    JOIN states ON cities.state_id = states.id \
+    ORDER BY cities.id ASC")
 
     for row in cur.fetchall():
         print(row)
