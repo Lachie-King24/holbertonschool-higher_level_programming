088a832bb21bfea06e6995c8760de54ab8248103
Lachie King <kingy00112233@gmail.com>
2026-03-07 12:14:31 +1100

    add task 10

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""List all State objects from the database hbtn_0e_6_usa"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    # Get arguments: mysql username, password, database
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    # Connect to the MySQL server using SQLAlchemy
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            username, password, database)
+    )
+
+    # Create a configured "Session" class
+    Session = sessionmaker(bind=engine)
+
+    # Create a session
+    session = Session()
+
+    # Query all State objects, ordered by id
+    states = session.query(State).filter(
+        State.name.like('{state_name}')
+        ).order_by(State.id).all()
+
+    for state in states:
+        print(f"{state.id}")
+
+    # Close the session
+    session.close()
