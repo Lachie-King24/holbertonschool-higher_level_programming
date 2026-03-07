4897a5abe411cb8478c7738acf3938cd42bdf760
Lachie King <kingy00112233@gmail.com>
2026-03-07 13:05:02 +1100

    update

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -3,5 +3,5 @@ SELECT genres.name AS genre,
        COUNT(DISTINCT tv_show_genres.show_id) AS number_of_shows
 FROM genres
 JOIN tv_show_genres ON genres.id = tv_show_genres.genre_id
-GROUP BY genres.name
+GROUP BY genres.id
 ORDER BY number_of_shows DESC;
