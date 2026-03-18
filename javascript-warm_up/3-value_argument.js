060f18217e5d212a94263ea9ee840946bcd2d3ba
Lachie King <kingy00112233@gmail.com>
2026-03-18 01:55:00 +0000

    minor updates semistandard

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -3,6 +3,7 @@
 const arg = process.argv;
 
 if (arg[2] === undefined) {
-  console.log('No argument')
-} else {console.log(arg[2]);
+  console.log('No argument');
+} else {
+  console.log(arg[2]);
 }
