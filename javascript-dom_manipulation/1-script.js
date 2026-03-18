7db536db3eb6fd2d7094c4d699d0bc9b3037f72f
Lachie King <kingy00112233@gmail.com>
2026-03-18 23:48:57 +0000

    update 1-script

--- javascript-dom_manipulation/1-script.js
+++ javascript-dom_manipulation/1-script.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-const redHeader = document.getElementById('red_header')
+const redHeader = document.getElementById('red_header');
 const header = document.querySelector('header');
 
 redHeader.addEventListener('click', function() {
-  header.style.color = 'red';
+  header.style.color = '#FF0000';
 });
