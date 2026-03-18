ff60a8bd941b15e62798eb50a94a39e2905c4e2f
Lachie King <kingy00112233@gmail.com>
2026-03-18 01:27:42 +0000

    update semistandard 2-argument

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 
-// process.argv already automatically has 2 arguments. 
+// process.argv already automatically has 2 arguments.
 const count = process.argv.length - 2;
 
 if (count === 0) {
-    console.log('No argument');
+  console.log('No argument');
 } else if (count === 1) {
-    console.log('Argument found');
+  console.log('Argument found');
 } else {
-    console.log('Arguments found');
+  console.log('Arguments found');
 }
